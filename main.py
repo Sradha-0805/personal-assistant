d4d315a11c0a48cad81cfe9c58838118ae9e7c21
christina <christinajacob06@mail.com>
2021-12-06 22:17:28 +0530

    features added

---
+++ main.py
@@ -0,0 +1,58 @@
+import speech_recognition as sr
+import time
+import os
+from time import ctime
+import webbrowser
+import time
+import random
+from gtts import  gTTS
+
+r=sr.Recognizer()
+
+def record_audio(ask = False):
+    with sr.Microphone() as source:
+        if ask:
+            Doremon_speak(ask)
+        
+        audio=r.listen(source)
+        voice_data = ''
+        try:
+            voice_data=r.recognize_google(audio)
+            
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that')
+        except sr.RequestError:
+            print('Sorry, my speech service is down')
+        return voice_data  
+def Doremon_speak(audio_string):
+    tts = gTTS(text = audio_string, lang = 'en')
+    r = random.randint(1,1000000)
+    audio_file = 'audio-' + str(r) + '.mp3' 
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)     
+    
+def respound(voice_data):
+    if 'what is your name' in voice_data:
+       Doremon_speak('My name is Doremon')
+    if 'what is the time' in voice_data:
+        print(ctime())     
+    if 'search' in voice_data:
+        search = record_audio('What do you want to search for?') 
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        Doremon_speak('Here is what I found for' + search)       
+    if 'find location' in voice_data:
+        location = record_audio('What is the location?') 
+        url = 'https://google.nl/maps/place/' +location + '/&amp;'
+        webbrowser.get().open(url)
+        Doremon_speak('Here is the location of ' + location)                
+    if "exit" in voice_data:
+        exit()
+time.sleep(1)          
+print('How can I help you?')
+while 1:
+    voice_data = record_audio()
+    respound(voice_data)
+
